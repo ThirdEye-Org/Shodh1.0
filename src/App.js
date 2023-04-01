a1ec7ed24ba4fcd5cef658342e8e2081bc842f14
ANSH GOYAL <72507723+Ansh1693@users.noreply.github.com>
2023-04-01 14:26:05 +0530

    Validator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import { useState } from "react";
 import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Validator from "./components/Validator";
 
 function App() {
   
   return (
     <div className="bg-white">
       <Navbar />
-      {/* <Profile /> */}
+      <Validator />
+      {/* <Validator /> */}
     </div>
   );
 }
